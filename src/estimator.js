9f7a45add2a49ca3bb4fd0f35bf99f669c78e986
lateo <lateo@pop-os.localdomain>
2020-04-09 03:03:36 +0000

    fixing indentation issues

--- src/estimator.js
+++ src/estimator.js
@@ -1,57 +1,24 @@
-const covid19ImpactEstimator = (data) => {
-    _currentlyInfected = currentlyInfected(data);
-    _currentlyInfectedForSeverImpact = currentlyInfectedForSeverImpact(data);
-    _Impact = infectionByRequestedTime(currentlyInfected, data);
-    _severeInfectionRequestedTime = infectionByRequestedTime(_currentlyInfectedForSeverImpact, data)
-    _totalInfectionRequestedTime =  totalInfectionByRequestedTime(_Impact, _severeInfectionRequestedTime); 
-    _severeCasesByRequestedTime = 0.15 * _totalInfectionRequestedTime;
-    _totalHospitalBedsAvailable = totalHospitalBeds(_severeCasesByRequestedTime, data)
-    _casesForICUByRequestedTime = 0.05 * _totalInfectionRequestedTime;
-    _casesForVentilatorsByRequestedTime = 0.02 * _totalInfectionRequestedTime;
-    _dollarsInFlight = dollarsInFlight(_totalInfectionRequestedTime, data);
-    
-    return {
-        data : data,
-        impact: {
-            currentlyInfected: _currentlyInfected,
-            infectionByRequestedTime: _Impact
-        },
-        severeImpact: {
-            currentlyInfected: _currentlyInfectedForSeverImpact,
-            infectionByRequestedTime: _severeInfectionRequestedTime
-        },
-        severeCasesByRequestedTime: _severeCasesByRequestedTime,
-       hospitalBedsByRequestedTime: _totalHospitalBedsAvailable,
-       casesForICUByRequestedTime: _casesForICUByRequestedTime,
-       casesForVentilatorsByRequestedTime: _casesForVentilatorsByRequestedTime,
-       dollarsInFlight: _dollarsInFlight
-    }
-};
-
-export default covid19ImpactEstimator;
-
-
 // function for currently infected people
-function currentlyInfected (_data) {
+function currentlyInfected(_data) {
     return _data.reportedCases * 10;
 }
 
-function currentlyInfectedForSeverImpact (_data) {
+function currentlyInfectedForSeverImpact(_data) {
     return _data.reportedCases * 50;
 }
 
-function infectionByRequestedTime (_currentlyInfected, _data) {
+function infectionByRequestedTime(_currentlyInfected, _data) {
     factor = 1;
     // converting all period types to days
     // and finding the factor
-    if(_data.periodType === "days") {
-        factor = Math.round(data.timeToElapse / 3); 
+    if (_data.periodType === "days") {
+        factor = Math.round(data.timeToElapse / 3);
     } else if (_data.periodType === "weeks") {
         days = Math.round(data.timeToElapse * 7);
         factor = Math.round(days / 3);
-    }else if (_data.periodType === "months") {
+    } else if (_data.periodType === "months") {
         days = Math.round(data.timeToElapse * 4 * 7);
-        factor = Math.round(days /3);
+        factor = Math.round(days / 3);
     }
     return factor * _currentlyInfected * 2;
 }
@@ -64,7 +31,7 @@ function totalInfectionByRequestedTime(_impact, _severe) {
 
 function hospitalBedsByRequestedTime(_severeCasesinfection, _data) {
     beds_used = 0.65 * _data.totalHospitalBeds;
-    remaining_beds = _data.totalHospitalBeds - beds_used; 
+    remaining_beds = _data.totalHospitalBeds - beds_used;
 
     return remaining_beds - _severeCasesinfection;
 }
@@ -73,3 +40,37 @@ function hospitalBedsByRequestedTime(_severeCasesinfection, _data) {
 function dollarsInFlight(_infectionRequestedTime, _data) {
     return _infectionRequestedTime * _data.avgDailyIncomeInUSD * _data.avgDailyIncomePopulation * _data.timeToElapse;
 }
+
+
+const covid19ImpactEstimator = (data) => {
+    _currentlyInfected = currentlyInfected(data);
+    _currentlyInfectedForSeverImpact = currentlyInfectedForSeverImpact(data);
+    _Impact = infectionByRequestedTime(currentlyInfected, data);
+    _severeInfectionRequestedTime = infectionByRequestedTime(_currentlyInfectedForSeverImpact, data)
+    _totalInfectionRequestedTime = totalInfectionByRequestedTime(_Impact, _severeInfectionRequestedTime);
+    _severeCasesByRequestedTime = 0.15 * _totalInfectionRequestedTime;
+    _totalHospitalBedsAvailable = totalHospitalBeds(_severeCasesByRequestedTime, data)
+    _casesForICUByRequestedTime = 0.05 * _totalInfectionRequestedTime;
+    _casesForVentilatorsByRequestedTime = 0.02 * _totalInfectionRequestedTime;
+    _dollarsInFlight = dollarsInFlight(_totalInfectionRequestedTime, data);
+
+    return {
+        data: data,
+        impact: {
+            currentlyInfected: _currentlyInfected,
+            infectionByRequestedTime: _Impact
+        },
+        severeImpact: {
+            currentlyInfected: _currentlyInfectedForSeverImpact,
+            infectionByRequestedTime: _severeInfectionRequestedTime
+        },
+        severeCasesByRequestedTime: _severeCasesByRequestedTime,
+        hospitalBedsByRequestedTime: _totalHospitalBedsAvailable,
+        casesForICUByRequestedTime: _casesForICUByRequestedTime,
+        casesForVentilatorsByRequestedTime: _casesForVentilatorsByRequestedTime,
+        dollarsInFlight: _dollarsInFlight
+    }
+};
+
+export default covid19ImpactEstimator;
+
